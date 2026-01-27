cff905daf2aa27ac5f33aed70d74d86b43504e6a
badalprasadsingh <badal@datazip.io>
2026-01-27 18:22:33 +0530

    add cookies and catalog registration commands
    Signed-off-by: badalprasadsingh <badal@datazip.io>

---
+++ conf-example/readme.md
@@ -0,0 +1,34 @@
+### Step 1
+Change directory name from `conf-example` to `conf`
+
+### Step 2
+Rebuild module `amoro-ams` and start amoro
+
+Stay inside the root directory of `olake-amoro` and run
+```bash
+./run.sh
+```
+
+### Step 3
+Register a Catalog into `olake-amoro`. 
+
+There is a `destination.json` example in config directory. There is a `database-filer` and `table-filter` in that file, to filter out the necessary tables and databases.
+
+Generate the `cookies.txt` (server-token). Everytime you start amoro (`./run.sh` command), you need to run this command to refresh the `cookies.txt` file.
+
+```bash
+./run.sh cookies
+```
+
+Register the Catalog
+
+```bash
+./run.sh register --destination <absolute_path/destination.json>
+```
+
+### Note
+
+`./run.sh` will build the `amoro-ams` module along with its dependencies every time when you run it, in case you want to disable it comment out this line:
+```bash
+./mvnw install -DskipTests -pl amoro-ams -am
+```
--- run.sh
+++ run.sh
@@ -1,23 +1,4 @@
 #!/bin/bash
-#
-# Licensed to the Apache Software Foundation (ASF) under one
-# or more contributor license agreements.  See the NOTICE file
-# distributed with this work for additional information
-# regarding copyright ownership.  The ASF licenses this file
-# to you under the Apache License, Version 2.0 (the
-# "License"); you may not use this file except in compliance
-# with the License.  You may obtain a copy of the License at
-#
-#     http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
-#
-# Quick script to run AMS for local development
-#
 
 set -e
 
@@ -27,6 +8,60 @@ cd "$SCRIPT_DIR"
 export AMORO_HOME="$SCRIPT_DIR"
 export AMORO_CONF_DIR="$SCRIPT_DIR/conf"
 export AMORO_LOG_DIR="$SCRIPT_DIR/logs"
+
+command="$1"
+
+if [[ "$command" == "cookies" ]]; then
+  if [[ -f cookies.txt ]]; then
+    rm -f cookies.txt
+  fi
+
+  curl -s -X POST http://localhost:1630/api/ams/v1/login \
+    -H "Content-Type: application/json" \
+    -H "X-Request-Source: Web" \
+    -c cookies.txt \
+    -d '{"user": "admin", "password": "admin"}'
+
+  echo
+  echo "cookies.txt created successfully"
+  exit 0
+fi
+
+# register the catalog from destination.json
+if [[ "$command" == "register" ]]; then
+  shift
+
+  DESTINATION=""
+
+  while [[ $# -gt 0 ]]; do
+    case "$1" in
+      --destination)
+        DESTINATION="$2"
+        shift 2
+        ;;
+      *)
+        echo "Unknown option: $1"
+        exit 1
+        ;;
+    esac
+  done
+
+  if [[ -z "$DESTINATION" ]]; then
+    echo "Error: --destination <absolute_path> is required"
+    exit 1
+  fi
+  
+  curl -X POST http://localhost:1630/api/ams/v1/catalogs \
+    -H "Content-Type: application/json" \
+    -H "X-Request-Source: Web" \
+    -b cookies.txt \
+    -d @$DESTINATION
+
+  echo
+  echo "Successfully Registered"
+  
+  exit 0
+fi
 
 
 ./mvnw install -DskipTests -pl amoro-ams -am
@@ -40,6 +75,7 @@ echo "AMORO_CONF_DIR: $AMORO_CONF_DIR"
 echo "Web UI will be available at: http://localhost:1630"
 echo "Login: admin/admin"
 
+# run the AmoroServiceContainer
 java -Dlog4j.configurationFile=${AMORO_CONF_DIR}/log4j2.xml \
   -Dlog.home=${AMORO_LOG_DIR} \
   -Dlog.dir=${AMORO_LOG_DIR} \
